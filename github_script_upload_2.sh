6026e9b1a3b8d40cf981610dfe92685b8eebd027
geetha-222 <50653158+geetha-222@users.noreply.github.com>
2020-06-05 18:06:45 +0530

    Add files via upload

---
+++ github_script_upload_2.sh
@@ -0,0 +1,9 @@
+echo "The 'helloworld' file is reversed:"
+rev helloworld
+declare -i num=0
+echo "Enter the number that is to be factored:"
+read num
+factor $num
+echo "Enter a string that is to be printed:"
+read str
+yes $str
